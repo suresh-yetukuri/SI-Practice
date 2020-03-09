238b37ecc1a717d16f82f002f2f4972b9f5650c7
Suresh Yetukuri <Suresh.Yetukuri@kofax.com>
2020-03-09 19:14:15 +0530

    Updated Code: 09-03-2020|19:14

--- Beginner/Beginner.cpp
+++ Beginner/Beginner.cpp
@@ -163,7 +163,15 @@ Initially we can pass INT_MIN as xLastData
 Node* Distinct(Node* pNode, const int xLastData)
 {
     if ((nullptr == pNode) || (nullptr == pNode->pNext))
+    {
+        if (xLastData == pNode->Data)
+        {
+            delete(pNode);
+            pNode = nullptr;
+        } 
         return pNode;
+    }
+        
 
     if (xLastData == pNode->Data)
     {
@@ -184,23 +192,225 @@ elements.
 */
 Node* Unique(Node* pNode)
 {
+    if ((nullptr == pNode) || (nullptr == pNode->pNext))
+        return pNode;
 
-    return nullptr;
+    if (pNode->Data == pNode->pNext->Data)
+    {
+        // Look out for next valid node. 
+        // Till that time delete all nodes
+        const int NonUniqueVal = pNode->Data;
+        Node* pValidNode = pNode;
+        while ((nullptr != pValidNode) && (NonUniqueVal == pValidNode->Data))
+        {
+            Node* pNodeTBD = pValidNode;
+            pValidNode = pValidNode->pNext;
+            delete(pNodeTBD); pNodeTBD = nullptr;
+        }
+
+        return Unique(pValidNode);
+    }
+
+    pNode->pNext = Unique(pNode->pNext);
+    return pNode;
 }
+
+/*
+Reverse the List
+Recursively
+*/
+Node* Reverse(Node* pNode)
+{
+    if ((nullptr == pNode) || (nullptr == pNode->pNext))
+        return pNode;
+
+    Node* pRHeadNode = Reverse(pNode->pNext);
+    pNode->pNext->pNext = pNode;
+    pNode->pNext = nullptr;
+    return pRHeadNode;
+}
+
+/*
+Swap In Pairs
+*/
+Node* SwapInPairs(Node* pNode)
+{
+    if ((nullptr == pNode) || (nullptr == pNode->pNext))
+        return pNode;
+
+    // Swap Nodes
+    Node* pPairHeadNode = pNode->pNext;
+    pNode->pNext = pPairHeadNode->pNext;
+    pPairHeadNode->pNext = pNode;
+
+    pNode->pNext = SwapInPairs(pNode->pNext);
+    return pPairHeadNode;
+}
+
+/*
+Find Mid of the Linked List
+*/
+Node* FindMid(Node* pHead, bool IsLower)
+{
+    Node* pSlow = pHead;
+    Node* pFast = pHead;
+    Node* pSPrev = nullptr;
+    while ((nullptr != pFast) && (nullptr != pFast->pNext))
+    {
+        pSPrev = pSlow;
+        pSlow = pSlow->pNext;
+        pFast = pFast->pNext->pNext;
+    }
+
+    if(IsLower && (nullptr == pFast))
+        return pSPrev;
+     
+    return pSlow;
+}
+
+/*
+Check  if given Linked List is Palindrome
+or not
+*/
+bool IsPalindrome(Node* pHead)
+{
+    if ((nullptr == pHead) || (nullptr == pHead->pNext))
+        return true;
+
+    Node* pMid = FindMid(pHead, true);
+    Node* pLeft = pHead;
+    Node* pRight = pMid->pNext;
+    pRight = Reverse(pRight);
+    Node* pRightCurrent = pRight;
+
+    bool IsPalindrome = true;
+    while (nullptr != pRightCurrent)
+    {
+        if (pLeft->Data != pRightCurrent->Data) {
+            IsPalindrome = false;
+            break;
+        }
+
+        pLeft = pLeft->pNext;
+        pRightCurrent = pRightCurrent->pNext;
+    }
+
+    pRight = Reverse(pRight);
+    pMid->pNext = pRight;
+    return IsPalindrome;
+}
+
+/*
+Merge two sorted linked list
+*/
+Node* Merge(Node* pLeft, Node* pRight)
+{
+    if (nullptr == pLeft)
+        return pRight;
+    
+    if (nullptr == pRight)
+        return pLeft;
+
+    Node* pHead = nullptr;
+    Node* pCurrent = nullptr;
+    if (pLeft->Data < pRight->Data) {
+        pHead = pLeft;
+        pLeft = pLeft->pNext;
+    } else {
+        pHead = pRight;
+        pRight = pRight->pNext;
+    }
+
+    pCurrent = pHead;
+    while ((nullptr != pLeft) && (nullptr != pRight))
+    {
+        if (pLeft->Data < pRight->Data) {
+            pCurrent->pNext = pLeft;
+            pLeft = pLeft->pNext;
+        } else {
+            pCurrent->pNext = pRight;
+            pRight = pRight->pNext;
+        }
+        pCurrent = pCurrent->pNext;
+    }
+
+    if (nullptr != pLeft)
+        pCurrent->pNext = pLeft;
+    else
+        pCurrent->pNext = pRight;
+
+    return pHead;
+}
+
+/*
+Merge Sort
+*/
+Node* MergeSort(Node* pHead)
+{
+    if ((nullptr == pHead) || (nullptr == pHead->pNext))
+        return pHead;
+
+    Node* pMid = FindMid(pHead, true);
+    Node* pRight = pMid->pNext;
+    pMid->pNext = nullptr;
+    return Merge(MergeSort(pHead), MergeSort(pRight));
+}
+
+/*
+Reverse Linked List at m & n position
+*/
+Node* ReverseBetween(Node* pNode, int m, int n)
+{
+    if ((nullptr == pNode) || (nullptr == pNode->pNext))
+        return pNode;
+
+    if (1 == m)
+    {
+        Node* pLeft = pNode;
+        Node* pRight = pLeft;
+        while (1 < n) 
+        {
+            pRight = pRight->pNext;
+            --n;
+        }
+
+        Node* pRemaining = pRight->pNext;
+        pRight->pNext = nullptr;
+        pRight = Reverse(pLeft);
+        pLeft->pNext = pRemaining;
+        return pRight;
+    }
+
+    pNode->pNext = ReverseBetween(pNode->pNext, m - 1, n - 1);
+    return pNode;
+}
+
 
 
 int main()
 {
-    vector<int> oInput{ 4, 1, 3, 4, 2, 5, 4, 4, 1, 1, 4, 6, 7 };
-    sort(oInput.begin(), oInput.end());
-    Node* pHead = CreateLinkedList(oInput, 0);
+    vector<int> oLeft{ 3, 5, 2, 1, 4, 3, 9, 7, 8, 5, 6 };
+    //vector<int> oRight{ 2, 4, 6, 8, 10 };
+    // sort(oInput.begin(), oInput.end());
+    Node* pLeft = CreateLinkedList(oLeft, 0);
+    Node* pHead = MergeSort(pLeft);
+    PrintLinkedList(pHead);
+    cout << endl;
+    pHead = ReverseBetween(pHead, 4, 9);
+    PrintLinkedList(pHead);
+    cout << endl;
+
+
+
+    cout << IsPalindrome(pHead);
+    pHead = SwapInPairs(pHead);
     PrintLinkedList(pHead);
     cout << endl;
     PrintLinkedListReverse(pHead);
     cout << endl;
-    pHead = Distinct(pHead, INT_MIN);
-
-
+    pHead = Distinct(pHead, INT_MAX);
+    PrintLinkedList(pHead);
+    cout << endl;
 
     pHead = DeleteAll(pHead, 4);
     PrintLinkedList(pHead);
@@ -208,7 +418,6 @@ int main()
     pHead = DeleteAll(pHead, 5);
     PrintLinkedList(pHead);
     cout << endl;
-
 
 
     pHead = Insert(pHead, 3, 3);
